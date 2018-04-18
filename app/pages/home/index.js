bdd4d1b8713a6406a10f9eea8771e6e640203bdb
leslie233 <459858721@qq.com>
2018-04-18 17:45:53 +0800

    攻击碰撞检测

--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -26,6 +26,7 @@ export default class Home extends Component {
 			this.setState({ lock: true});
 			let self = this
 			arrKey[e.keyCode] = true
+			this.effect(e)
 			//单个键
 			switch (e.keyCode){
 				case 68:
@@ -37,9 +38,6 @@ export default class Home extends Component {
 						}, i*80);
 					}
 					break;
-				// case 87:
-				// m.style.top = m.offsetTop-10+"px";
-				// break;
 				case 65:
 					for(let i=1;i<=6;i++){
 						setTimeout(function(){ 
@@ -66,7 +64,7 @@ export default class Home extends Component {
 						m.src = Stand; 
 					}, 200);
 
-				break;
+					break;
 				case 75:
 					if(!arrKey[83]){
 						for(let i=1;i<=5;i++){
@@ -76,7 +74,7 @@ export default class Home extends Component {
 							}, i*100);
 						}
 					}	
-				break;
+					break;
 				case 32:
 					for(let i=1;i<=2;i++){
 						setTimeout(function(){ 
@@ -95,7 +93,7 @@ export default class Home extends Component {
 						}, i*100);
 					}
 
-				break;
+					break;
 
 				default:
 	           		self.setState({ lock: false});
@@ -128,6 +126,13 @@ export default class Home extends Component {
 							boom.style.display = "block"
 							let fly = setInterval(function(){
 								boom.style.left = boom.offsetLeft+15+"px"
+								if(player2.offsetLeft - boom.offsetLeft <= 48){
+									clearInterval(fly);
+									boom.style.display = "none"
+									blood2 = blood2 - 20
+									bar.style.clip = `rect(0 ${blood2}px 50px 0)`
+									self.attacked()
+								}
 								if(boom.offsetLeft>730){
 									clearInterval(fly);
 									boom.style.display = "none"
@@ -138,10 +143,27 @@ export default class Home extends Component {
 				}
 
 			}
+			if(arrKey[83]&&arrKey[65]&&arrKey[75]&&!this.state.wait){
+				this.setState({ wait: true});
+				for(let i=1;i<=5;i++){
+					setTimeout(function(){ 
+						m.style.left = m.offsetLeft+20+"px"
+						m.src = require(`../pic/jump_kick${i}.png`); 
+						if(player2.offsetLeft - m.offsetLeft <= 120){
+							blood2 = blood2 - 20
+							bar.style.clip = `rect(0 ${blood2}px 50px 0)`
+							self.attackedFly()
+						}
+						i === 5 && self.setState({ lock: false});
+					}, i*100);
+				}
+		
+			}
 			this.limit()
 		}
 		
 	}
+
 	keyup=(e)=>{
 		arrKey[e.keyCode] = false
 		switch(e.keyCode){
@@ -152,50 +174,144 @@ export default class Home extends Component {
 			case 74:
 				this.setState({ lock: false});
 				break;	
+			case 75:
+				//this.setState({ lock: false});
+				break;	
 			default:	
 				break;
 		}	
 		if(!arrKey[83] || !arrKey[68]){
 			this.setState({ boomTime: false});
 		}
-	}
-	limit=()=>{
-		if(m.offsetLeft<=0){
-			m.style.left=0
+		if(!arrKey[83] || !arrKey[65]){
+			this.setState({ wait: false});
 		}
-		if(m.offsetLeft>=680){
-			m.style.left=680+'px'
+	}
+
+	limit=()=>{
+		if(m.offsetLeft<=36){
+			m.style.left=36+'px'
+		}
+		if(m.offsetLeft>=612){
+			m.style.left=612+'px'
 		}
 		if(m.offsetTop<=0){
 			m.style.top=0
 		}
 		if(m.offsetTop>=380){
 			m.style.top=380+'px'
-		}       
+		}  
+		if(player2.offsetLeft>=680){
+			player2.style.left=680+'px'
+		}
+
     }
-   //  wait=()=>{
-   //  	if(!this.state.wait){
-   //  		for(let i=1;i<=5;i++){
-			// 	setInterval(()=>{ 
-			// 		m.src = require(`../pic/wait${i}.png`); 
-			// 		i === 5 && this.setState({ lock: false});
-			// 	}, i*180);
-			// }
-   //  	}			
-   //  }
+
+    ko=()=>{
+    	let self = this
+    	this.refs.ko.style.display = "block";
+    	for(let i=1;i<=4;i++){
+			setTimeout(function(){ 
+				player2.style.left = player2.offsetLeft+8+"px"
+				self.limit()
+				player2.src = require(`../pic/fall${i}.png`); 
+			}, i*100);
+		}
+    }
+
+    attacked = ()=>{
+    	if(blood2 <= 172){
+			this.ko()
+		}
+		else{
+			for(let i=1;i<=2;i++){
+				setTimeout(function(){ 
+					player2.src = require(`../pic/beattacked${i}.png`); 
+				}, i*60);
+			}
+			setTimeout(function(){ 
+				player2.src = require('../pic/stand.png'); 
+			}, 400);
+		}
+    }
+
+    attackedFly = ()=>{
+    	if(blood2 <= 172){
+			this.ko()
+		}
+		else{
+			let self = this
+	    	for(let i=1;i<=4;i++){
+				setTimeout(function(){ 
+					player2.style.left = player2.offsetLeft+8+"px"
+					self.limit()
+					player2.src = require(`../pic/fall${i}.png`); 
+				}, i*100);
+			}
+			setTimeout(function(){ 
+				player2.src = require('../pic/stand.png'); 
+			}, 1500);
+		}
+    }
+
+    effect = e =>{
+		let distance = player2.offsetLeft - m.offsetLeft
+		//console.log("p1",m.offsetLeft)
+		//console.log("p2",player2.offsetLeft)
+		switch (e.keyCode){
+			case 68:
+				if(distance <= 86){
+					for(let i=1;i<=6;i++){
+						setTimeout(function(){ 
+							player2.style.left = player2.offsetLeft+6+"px";
+							player2.src = require(`../pic/back${i}.png`); 
+						}, i*80);
+					}
+				}
+				break;
+			case 74:
+				if(distance <= 86){
+					blood2 = blood2 - 15
+					bar.style.clip = `rect(0 ${blood2}px 50px 0)`
+					this.attacked()
+				}
+				break;
+			case 75:
+				if(distance <= 116){
+					blood2 = blood2 - 15
+					bar.style.clip = `rect(0 ${blood2}px 50px 0)`
+					this.attacked()
+				}
+				break;
+			case 32:
+				break;
+			default:
+           		
+          		break; 		
+
+		}
+
+    }
 
 	componentDidMount(){
 		window.m =this.refs.move
+		window.player2 =this.refs.player2
 		window.boom =this.refs.boom
+		window.bar =this.refs.bar
+		window.blood2 = 322
 		window.arrKey = {}
 		window.addEventListener('keydown', this.move)
 		window.addEventListener('keyup', this.keyup)
 		//this.wait()
 	}
+
     render(){
         return <div className="container">
+        	<img className="ko" ref = "ko" src={require('../pic/KO.png')} />
+        	<img className="bar" ref = "bar" src={require('../pic/bar.gif')} />
         	<img className="move" ref = "move" src={require('../pic/go1.png')} />
         	<img className="boom" ref = "boom" src={require('../pic/boom.png')} />
+        	<img className="player2" ref = "player2" src={require('../pic/stand.png')} />
         </div>
     }
 }
